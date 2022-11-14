ecb160390c1cd0a7766df11f2ca854ccb784e82d
王佳培 <jackie.wjp@aliyun.com>
2022-11-14 20:26:16 +0800

    init 目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>扫雷</div>
+  <div>
+    <IMinesweeperVue msg="扫雷 2.0"></IMinesweeperVue>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IMinesweeperVue from "./components/IMinesweeper.vue";
+</script>
 
 <style scoped></style>
---
+++ src/components/IMinesweeper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  msg: String,
+});
+</script>
+
+<style scoped></style>
